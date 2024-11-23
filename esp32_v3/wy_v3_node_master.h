5c5b21b9143d5d3bfc75026cc868d1a8d089254b
goodfellasthai <goodfellasthai@gmail.com>
2024-11-24 00:09:17 +0700

    Definitely checkpoint centralised globals

--- esp32_v3/wy_v3_node_master.h
+++ esp32_v3/wy_v3_node_master.h
@@ -4,7 +4,7 @@
 // can't put the main .cpp reliant libraries in .h as would conflict with other libraries using the same functions
 // the .h always needs called first in the .cpp module file so that the application knows the functions it can use for the board
 #include "wyconfig.h"
-#include "globals.h"
+#include "../master/globals.h"
 
 void wy_v3_node_master_setup();
 void wy_v3_node_master_loop();
